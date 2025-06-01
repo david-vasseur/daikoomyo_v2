2fa61e2dd92928057972c0489176311112eb3ef7
David <p5y4@laposte.net>
2025-06-01 13:24:29 +0200

    new features

--- src/app/back-office/planing/page.tsx
+++ src/app/back-office/planing/page.tsx
@@ -1,6 +1,8 @@
 import { getAllMeet } from '@/lib/pages/meet'
 import React from 'react'
 
+export const dynamic = "force-dynamic";
+
 const page = async () => {
 
     const meets = await getAllMeet();
@@ -16,20 +18,19 @@ const page = async () => {
     return (
     <div className="py-20 bg-pink-200 h-[100vh] flex flex-col gap-10 items-center">
         <h1>Mon planing:</h1>
-        <div>
+        <div className="flex flex-col gap-5">
             {meets.map((meet, index) => {
                 return (
-                    <div key={index}>
+                    <div key={index} className="p-5 bg-gray-200 rounded-lg shadow-2xl">
                         <h2>Rendez-vous avec {meet.name}</h2>
                         <h3>{meet.dateStart && `le ${new Date(meet.dateStart).toLocaleDateString('fr-FR')} à ${new Date(meet.dateStart).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' })}`}</h3>
                         <h3>à {meet.address}</h3>
                     </div>
                 )
-                
             })}
         </div>
     </div>
     )
 }
 
-export default page
+export default page;
