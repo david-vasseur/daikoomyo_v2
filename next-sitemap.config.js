db336404e6c743549b02c1a3275ab82a3ab8be44
David <p5y4@laposte.net>
2025-06-02 22:42:50 +0200

    adding validation form

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.daikoomyo.fr',
-  generateRobotsTxt: true, 
-  sitemapSize: 5000,      
+  generateRobotsTxt: true,
+  exclude: ['/back-office', '/back-office/*'], 
+  sitemapSize: 5000,
   changefreq: 'weekly',
   priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/back-office', '/back-office/*'],
+      },
+    ],
+  },
 };
