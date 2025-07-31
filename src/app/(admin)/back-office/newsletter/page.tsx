1ef62432bb2ba87ecfd4f994695033f18c69f872
David <p5y4@laposte.net>
2025-07-31 20:23:00 +0200

    adding newsletter

--- src/app/(admin)/back-office/newsletter/page.tsx
+++ src/app/(admin)/back-office/newsletter/page.tsx
@@ -21,7 +21,10 @@ const page = async () => {
         <BackButton />
         <h1 className="text-5xl text-center font-bold mt-20 text-shadow-lg text-shadow-pink-300 border-b-pink-300 border-b-2">Mes abonnés à la newsletter:</h1>
         <div className="flex flex-col gap-5">
-            {emails.emails !== undefined && emails.emails?.length >= 1 && emails.emails.map((email, index) => {
+             
+            {
+            // eslint-disable-next-line
+            emails.emails !== undefined && emails.emails?.length >= 1 && emails.emails.map((email, index) => {
                 return (
                     <div key={index} className={`p-5 rounded-lg shadow-2xl`}>
                         <h2><span className="font-extrabold">{email.email.toUpperCase()}</span></h2>
